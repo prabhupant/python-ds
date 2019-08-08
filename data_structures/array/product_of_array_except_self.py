ddae75ba3ba27973f458651bd7d153612492045a
Prabhu Pant <prabhupant09@gmail.com>
2019-08-08 22:11:10 +0530

    new questions

---
+++ data_structures/array/product_of_array_except_self.py
@@ -0,0 +1,14 @@
+def product(arr):
+    prods = [1] * len(arr)
+
+    temp = 1
+
+    for i in range(len(arr)):
+        prods[i] = temp
+        temp = temp * prods[i]
+
+    temp = 1
+
+    for i in reversed(range(len(arr))):
+        prods[i] = prods[i] * temp
+        temp = temp * arr[i]
--- data_structures/array/square_of_sorted_array.py
+++ data_structures/array/square_of_sorted_array.py
@@ -1,2 +1,33 @@
 def square(arr):
+    n = len(arr)
+    j = 0
+    
+    # Counting the number of negative elements 
+    while j < n and arr[j] < 0:
+        j += 1
+    
+    i = j - 1 # index of last negative element
+    ans = []
+
+    while 0 <= i and j < n:
+        if arr[i] ** 2 < arr[j] ** 2:
+            ans.append(arr[i] ** 2)
+            i -= 1
+        else:
+            ans.append(arr[j] ** 2)
+            j += 1
+
+    while i >= 0:
+        ans.append(arr[i] ** 2)
+        i -= 1
+    
+    while j < n:
+        ans.append(arr[j] ** 2)
+        j += 1
+
+    return ans
+
+
+
+
 
