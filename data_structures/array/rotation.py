9ae8a5eb7f1ae13d640600f6282516b0a268dc97
Prabhu Pant <prabhupant09@gmail.com>
2019-08-31 19:36:05 +0530

    Create rotation.py

---
+++ data_structures/array/rotation.py
@@ -0,0 +1,24 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a%b)
+
+
+def rotate(arr, d):
+    n = len(arr)
+    g = gcd(n, d)
+
+    for i in range(g):
+        temp = arr[i]
+        j = i
+
+        while 1:
+            k = j + d
+            if k >= n:
+                k = k -n
+            if k == i:
+                break
+            arr[j] = arr[k]
+            j = k
+        
+        arr[j] = temp
