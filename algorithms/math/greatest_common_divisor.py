183f1f1b154dd3064b521b49fcbe7be45c394a91
Mohit Saini <sainimohit23@gmail.com>
2019-10-07 16:42:20 +0530

    added gcd

---
+++ algorithms/math/greatest_common_divisor.py
@@ -0,0 +1,29 @@
+"""
+High Level Description:
+Given two input integers, find their Greatest Common Divisor.
+
+Time Complexity:
+O(log(n))
+"""
+# Iterative Solution
+def gcd(x, y):
+	if x==0:
+		return y
+	if y==0:
+		return x
+	
+	while x%y != 0:
+		rem = x%y;
+		x = y
+		y = rem
+
+	return y
+
+# Recursive Solution
+def gcd(x, y):
+	if x==0:
+		return y
+	if y==0:
+		return x
+	
+	return gcd(y, x%y)
