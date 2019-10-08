0baff8452bc848895e14db60ad55fc26870a4e70
Jay Welborn <jesse.welborn@gmail.com>
2019-10-08 09:24:29 -0400

    Update merge_list_of_linked_lists.py

--- data_structures/linked_list/merge_list_of_linked_lists.py
+++ data_structures/linked_list/merge_list_of_linked_lists.py
@@ -4,7 +4,7 @@
 #         self.val = x
 #         self.next = None
 
-def merge_two_lists(self, l1: ListNode, l2: ListNode):
+def merge_two_lists(l1: ListNode, l2: ListNode):
     if not l1 and not l2:
         return
     elif not l2:
@@ -13,25 +13,25 @@ def merge_two_lists(self, l1: ListNode, l2: ListNode):
         return l2
 
     if (l1.val < l2.val):
-        l1.next = self.merge_two_lists(l1.next, l2)
+        l1.next = merge_two_lists(l1.next, l2)
         return l1
-    l2.next = self.merge_two_lists(l1, l2.next)
+    l2.next = merge_two_lists(l1, l2.next)
     return l2
 
 
-def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+def mergeKLists(lists: List[ListNode]) -> ListNode:
     length = len(lists)
     if length == 0:
         return;
     elif length == 1:
         return lists[0]
     elif length == 2:
-        return self.merge_two_lists(lists[0], lists[1])
+        return merge_two_lists(lists[0], lists[1])
 
     mid = length // 2
 
     left_half = lists[:mid]
     right_half = lists[mid:]
 
-    return self.merge_two_lists(self.mergeKLists(left_half), self.mergeKLists(right_half))
+    return merge_two_lists(mergeKLists(left_half), mergeKLists(right_half))
    
