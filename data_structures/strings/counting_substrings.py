b085cbcfc4d34b48918a3ff42801b0682abb22cf
17-versha <41384378+17-versha@users.noreply.github.com>
2019-10-09 22:30:30 +0530

    I have added required comments

--- data_structures/strings/counting_substrings.py
+++ data_structures/strings/counting_substrings.py
@@ -1,4 +1,22 @@
 # cook your dish here
+'''
+Given a string S consisting of only 1s and 0s, find the number of substrings which start and end both in 1.
+In this problem, a substring is defined as a sequence of continuous characters Si, Si+1, ..., Sj where 1 ≤ i ≤ j ≤ N.
+Input:
+First line contains T, the number of testcases. Each testcase consists of N(the length of string) in one line and string in second line.
+
+Example
+Input:
+2
+4
+1111
+5
+10001
+
+Output:
+10
+3
+'''
 T = int(input())
 
 A = []
