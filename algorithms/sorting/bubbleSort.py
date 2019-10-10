262a0f3a6baa1926014514b2f03bc52493b3bfb9
Prabhu Pant <prabhupant09@gmail.com>
2019-10-10 22:14:25 +0530

    Update bubbleSort.py

--- algorithms/sorting/bubbleSort.py
+++ algorithms/sorting/bubbleSort.py
@@ -10,9 +10,9 @@ Bubble sort has a time complexity of О(n^2)
 """
 
 
-def bubbleSort(arr):
-    for i in range(0,len(arr)):
-        for j in range(0,len(arr)-1):
+def bubble_sort(arr):
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)-1):
             if (arr[j] > arr[j+1]):
                 arr[j] , arr[j+1] = arr[j+1] , arr[j]
     return arr
