b7228c1654f5863db7d497bb354c29058f66ca28
som-cs <sudhanshu253@gmail.com>
2019-10-10 16:53:16 +0530

    Added Bubble sort

---
+++ algorithms/sorting/bubbleSort.py
@@ -0,0 +1,18 @@
+"""
+High level explanation:
+Bubble sort:
+is a simple sorting algorithm that repeatedly steps through the list,
+compares adjacent elements and swaps them if they are in the wrong order.
+The pass through the list is repeated until the list is sorted.
+
+Time Complexity:
+Bubble sort has a time complexity of О(n^2)
+"""
+
+
+def bubbleSort(arr):
+    for i in range(0,len(arr)):
+        for j in range(0,len(arr)-1):
+            if (arr[j] > arr[j+1]):
+                arr[j] , arr[j+1] = arr[j+1] , arr[j]
+    return arr
