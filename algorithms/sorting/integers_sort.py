c48c932366eb84f2d7433fa818ffc026297302dc
Adán Hernández Baena <39740586+HBaena@users.noreply.github.com>
2019-10-10 22:30:47 -0500

    Removing libraries and add comments
    Adding explanation and important comments.

--- algorithms/sorting/integers_sort.py
+++ algorithms/sorting/integers_sort.py
@@ -1,64 +1,48 @@
-from numpy import array, random, zeros
+from random import randint
 # @author  HBaena
 # @profile https://github.com/HBaena/
 
 # Exclusive for integers
 # Dependeing of max and min values this algorithm will need a lot of memory
 # But less processor
-# 
-def count_sort_list(unsort:array, reverse:str=False) -> list:
+
+'''
+This algorithm only works with an integer array.
+basically, it creates a histogram with all possible numbers between the min and max 
+values of the array. Thus, if the histogram has a value not zero in any position, 
+it indicates if the number corresponding to the position is in the array and how many 
+times it is repeated too.
+'''
+def count_sort(unsort:list, reverse:str=False) -> list:
+    # Calculate min and max values
     min_val = min(unsort)
     max_val = max(unsort)
-
+    # create and empty array of all values between min and max values and it is started with zeros  
     histogram = [0]*(max_val - min_val + 1)
-
+    
     for n in unsort:
+        # Histogram is increased by one for each value in the unsorted array
+        # if n = min_values the position 0 is increased
+        # if n = min_values the position 1 and so on
         histogram[n - min_val] += 1 
 
     i = 0
+    # The out array must be of the same lenght of unsorted array
     out = [0]*len(unsort)
-
     if not reverse:
         direction = range(max_val - min_val, -1   , -1)
     else:
         direction = range(0, max_val - min_val + 1,  1)
     
     for  j in direction:
+        # if histogram is not zero, the values min_val + j is added to the out array
         while histogram[j]: 
             out[i] = min_val + j
             i += 1
             histogram[j] -= 1
     return out
 
-def count_sort_numpy(unsort:array, reverse:str=False) -> array:
-    min_val = unsort.min()
-    max_val = unsort.max()
+random_array = [randint(-1000, 1000) for _ in range(100)]
 
-    histogram = zeros(max_val - min_val + 1, dtype='int')
-
-    for n in unsort:
-        histogram[n - min_val] += 1 
-
-    i = 0
-    out = zeros(unsort.size, dtype='int')
-
-    if not reverse:
-        direction = range(max_val - min_val, -1   , -1)
-    else:
-        direction = range(0, max_val - min_val + 1,  1)
-    
-    for  j in direction:
-        while histogram[j]: 
-            out[i] = min_val + j
-            i += 1
-            histogram[j] -= 1
-    return out
-
-
-random_array = random.randint(-1000, 1000, 50, dtype='int')
-
-print(list(count_sort_numpy(random_array, reverse=False)))
-print(list(count_sort_numpy(random_array, reverse=True)))
-
-print(list(count_sort_list(random_array, reverse=False)))
-print(list(count_sort_list(random_array, reverse=True)))
+print(list(count_sort(random_array, reverse=False)))
+print(list(count_sort(random_array, reverse=True)))
