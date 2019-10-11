0f0e1fa3337d48863ed68bce7b2aabdaa90f7b73
Nishkarsh-Tripathi <56263978+Nishkarsh-Tripathi@users.noreply.github.com>
2019-10-12 00:20:44 +0530

    Added a edge case condition and test case

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -6,7 +6,17 @@ Best time scenario is when array is already sorted - O(n)
 def bubbleSort(array):
     n = len(array)
 
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
+    for i in range(n):   
+        swapped = False
+        for j in range(0, n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+        if not swapped:
+            break
+    return array
+
+
+# Driver code
+array = [14, 2, 5, 23, 56]
+print(bubbleSort(array))
