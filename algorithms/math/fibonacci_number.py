8cb2bd3de0692f77bd70c6adafcf6b302f52f103
Miguel Mendes <miguel.mdebrito@gmail.com>
2019-10-12 10:21:14 +0100

    Refactor fibonacci

--- algorithms/math/fibonacci_number.py
+++ algorithms/math/fibonacci_number.py
@@ -1,23 +1,39 @@
-def calc_fib(num):
-    while len(fib)<=num:
-        n = len(fib)
-        fib.append((fib[n-1]+fib[n-2]))
-        
-def main():
-    print("Enter the Position of the Number in the Sequence or \'0\' to Quit: ")
-    num = 0
-    fib = list()
-    fib.append(0)
-    fib.append(1)
-    while True:
-        num = int(input())
-        if(num<=0):
-            break
+def fibonacci(n):
+    """
+    Int fibonacci sequence ach number in the sequence is
+    the sum of the two numbers that precede it.
+    Like so: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, and so on.
 
-        if len(fib)<=num:
-            calc_fib(num)
+    :param n:
+    :return:
 
-        print('Fibonacci Number at Position '+str(num)+' is: '+str(fib[num]))
-        
-if __name__ == '__main__':
-    main()
+    Examples:
+    >>> fibonacci(0)
+    0
+    >>> fibonacci(1)
+    1
+    >>> fibonacci(7)
+    13
+    >>> fibonacci(10)
+    55
+
+    """
+    fn_2 = 0
+    fn_1 = 1
+
+    if n == 0:
+        return fn_2
+    if n == 1:
+        return fn_1
+    fn = 0
+    for _ in range(2, n + 1):
+        fn = fn_1 + fn_2
+        fn_2, fn_1 = fn_1, fn
+
+    return fn
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
