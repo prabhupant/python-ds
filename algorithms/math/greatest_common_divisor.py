538003511cd449b7511e06d717b714a05240ce13
Miguel Mendes <miguel.mdebrito@gmail.com>
2019-10-12 10:25:55 +0100

    Fix GCD

--- algorithms/math/greatest_common_divisor.py
+++ algorithms/math/greatest_common_divisor.py
@@ -1,29 +1,63 @@
-"""
-High Level Description:
-Given two input integers, find their Greatest Common Divisor.
+def iterative_gcd(x, y):
+    """
+    High Level Description:
+    Given two input integers, find their Greatest Common Divisor.
 
-Time Complexity:
-O(log(n))
-"""
-# Iterative Solution
-def gcd(x, y):
-	if x==0:
-		return y
-	if y==0:
-		return x
-	
-	while x%y != 0:
-		rem = x%y;
-		x = y
-		y = rem
+    Time Complexity:
+    O(log(n))
 
-	return y
+    :param x:
+    :param y:
+    :return:
 
-# Recursive Solution
-def gcd(x, y):
-	if x==0:
-		return y
-	if y==0:
-		return x
-	
-	return gcd(y, x%y)
+    Examples:
+    >>> iterative_gcd(8, 12)
+    4
+    >>> iterative_gcd(54, 24)
+    6
+
+    """
+    if x == 0:
+        return y
+    if y == 0:
+        return x
+
+    while x % y != 0:
+        rem = x % y
+        x = y
+        y = rem
+
+    return y
+
+
+def recursive_gcd(x, y):
+    """
+    High Level Description:
+    Given two input integers, find their Greatest Common Divisor.
+
+    Time Complexity:
+    O(log(n))
+
+    :param x:
+    :param y:
+    :return:
+
+    Examples:
+    >>> recursive_gcd(8, 12)
+    4
+    >>> recursive_gcd(54, 24)
+    6
+
+    """
+    if x == 0:
+        return y
+    if y == 0:
+        return x
+
+    return recursive_gcd(y, x % y)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
