8a7a45eb8e1128c9d8e849baad4d089d4b070767
Miguel Mendes <miguel.mdebrito@gmail.com>
2019-10-12 12:50:31 +0100

    Refactor bubble sort

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -1,12 +1,26 @@
-'''
-Bubble Sort worst time complexity occurs when array is reverse sorted - O(n^2)
-Best time scenario is when array is already sorted - O(n)
-'''
+def bubble_sort(arr):
+    """
+    Bubble Sort worst time complexity occurs when array is reverse sorted - O(n^2)
+    Best time scenario is when array is already sorted - O(n)
 
-def bubbleSort(array):
-    n = len(array)
+    :param arr:
+    :return: sorted array
+
+    Examples:
+    >>> bubble_sort([2, 3, 5, 0, 9, 1])
+    [0, 1, 2, 3, 5, 9]
+    """
+    n = len(arr)
 
     for i in range(n):
-        for j in range(0, n-i-1):
-            if array[j] > array[j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
