7bb42c1cdb9ad0341cb8fd36da68a7f468d15519
Miguel Mendes <miguel.mdebrito@gmail.com>
2019-10-12 18:52:55 +0100

    Refactor sieve

--- algorithms/math/sieve_of_eratosthenes.py
+++ algorithms/math/sieve_of_eratosthenes.py
@@ -1,18 +1,29 @@
-"""
-Description:
-Sieve of Eratosthenes is a very fast method (loglog(n)) of finding primes upto a given number.
-"""
-
-
 def sieve(n):
-    prime_list = []
-    for i in range(2, n+1):
-        if i not in prime_list:
-            print(i)
-            for j in range(i*i, n+1, i):
-                prime_list.append(j)
+    """
+    Description:
+    Sieve of Eratosthenes is a very fast method (loglog(n)) of finding primes upto a given number.
+    
+    :param n: int
+    :return List[int]: prime numbers up to n including n
+
+    Examples:
+    >>> sieve(10)
+    [2, 3, 5, 7]
+    >>> sieve(31)
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+    """
+    num_table = []
+    primes = []
+    for i in range(2, n + 1):
+        if i not in num_table:
+            primes.append(i)
+            for j in range(i * i, n + 1, i):
+                num_table.append(j)
+    return primes
 
 
-if __name__ == '__main__':
-    input_number = int(input("Provide a number upto which primes are to be found :"))
-    sieve(input_number)
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
