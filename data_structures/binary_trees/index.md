6427beb786f515f6bab8c58c3d8ca328ef43dd57
Elena Kutanov <elena.kutanov@gmail.com>
2019-10-12 20:44:52 +0200

    Added new questions for binary search tree

--- data_structures/binary_trees/index.md
+++ data_structures/binary_trees/index.md
@@ -1,3 +1,8 @@
 # Index of binary_trees
 
 * symmetric_binary_tree.py
+
+# Univalued Binary Tree
+Check if binary tree has the same value in each node.
+
+* univalued_binary_tree.py
---
+++ data_structures/binary_trees/univalued_binary_tree.py
@@ -0,0 +1,18 @@
+ class Tree:
+     def __init__(self, x):
+         self.value = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def isUnivalTree(self, root: Tree) -> bool:
+        vals = set()
+
+        def dfs(node):
+            if node:
+                vals.add(node.value)
+                dfs(node.left)
+                dfs(node.right)
+
+        dfs(root)
+        return len(vals) == 1
