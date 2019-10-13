a395cdef353bf32109566003b8329d2ee4460d61
Abhinav Hinger <abhinavhinger12@gmail.com>
2019-10-14 02:15:35 +0530

    Longest Increasing Subsequence with Binary Search

---
+++ algorithms/dynamic_programming/longest_increasing_sub_NlogN.py
@@ -0,0 +1,23 @@
+# O(nlogn) solution with binary search
+def lengthOfLIS(self, nums):
+
+        def binarySearch(sub, val):
+            lo, hi = 0, len(sub)-1
+            while(lo <= hi):
+                mid = lo + (hi - lo)//2
+                if sub[mid] < val:
+                    lo = mid + 1
+                elif val < sub[mid]:
+                    hi = mid - 1
+                else:
+                    return mid
+            return lo
+        
+        sub = []
+        for val in nums:
+            pos = binarySearch(sub, val)
+            if pos == len(sub):
+                sub.append(val)
+            else:
+                sub[pos] = val
+        return len(sub)
