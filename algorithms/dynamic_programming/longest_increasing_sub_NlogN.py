ead2d08fd05ea3fef2d62c4e804c3ff537d5f8e3
Abhinav Hinger <abhinavhinger12@gmail.com>
2019-10-14 02:23:01 +0530

    Added Solution approach

--- algorithms/dynamic_programming/longest_increasing_sub_NlogN.py
+++ algorithms/dynamic_programming/longest_increasing_sub_NlogN.py
@@ -1,3 +1,13 @@
+'''
+1.initial sub = [ ].
+2. traversing the nums:
+    a) if val > sub's all elements, then subsequence length increased by 1, sub.append(val);
+    b) if sub[i-1] < val < sub[i], then we find a smaller value, update sub[i] = val. Some of the elements stored in the sub[ ] are known subsequences, and the other part is elements of other possible new subsequences. However, the length of the known subsequences is unchanged.
+3.return the sub[ ]'s length.
+
+'''
+
+#Because of sub[ ] is incremental, we can use a binary search to find the correct insertion position.
 # O(nlogn) solution with binary search
 def lengthOfLIS(self, nums):
 
