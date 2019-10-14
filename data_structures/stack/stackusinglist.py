0a84472f9c964b32024389fc491bfe054abe6af3
Dhirendra <dhirendrachoudhary@yahoo.com>
2019-10-14 23:09:19 +0530

    letter of stack program added

---
+++ data_structures/stack/stackusinglist.py
@@ -0,0 +1,18 @@
+letters = []
+
+# Let's push some letters into our list
+letters.append('c')
+letters.append('a')
+letters.append('t')
+letters.append('g')
+
+# Now let's pop letters, we should get 'g'
+last_item = letters.pop()
+print(last_item)
+
+# If we pop again we'll get 't'
+last_item = letters.pop()
+print(last_item)
+
+# 'c' and 'a' remain
+print(letters) # ['c', 'a']
