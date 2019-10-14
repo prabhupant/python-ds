fe3a4829a8655d90caefe0f50f6345aed1cf617c
Ayushi <ayu123shi@gmail.com>
2019-10-14 14:28:52 +0530

    added divide_and_conquer algorithm

---
+++ algorithms/divide_and_conquer/binary_search.py
@@ -0,0 +1,14 @@
+def BinarySearch(lys, val):
+    first = 0
+    last = len(lys)-1
+    index = -1
+    while (first <= last) and (index == -1):
+        mid = (first+last)//2
+        if lys[mid] == val:
+            index = mid
+        else:
+            if val<lys[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    return index
---
+++ algorithms/divide_and_conquer/index.md
@@ -0,0 +1,3 @@
+# index of divide and conquer
+
+#binary_search
