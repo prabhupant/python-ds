78d16e31598ec39656b26c9b0653ecdf4d179baa
Oishika Pradhan <oishikapradhan@gmail.com>
2019-10-15 13:21:54 +0530

    used snake case

--- data_structures/graphs/TopoSort.py
+++ data_structures/graphs/TopoSort.py
@@ -7,38 +7,38 @@ class Graph:
         self.graph = defaultdict(list)
         self.V = vertices
 
-    def addEdge(self, u, v):
+    def add_edge(self, u, v):
         self.graph[u].append(v)
 
-    def topologicalSortUtil(self, v, visited, stack):
+    def topological_sort_util(self, v, visited, stack):
 
         visited[v] = True
 
         for i in self.graph[v]:
             if not visited[i]:
-                self.topologicalSortUtil(i, visited, stack)
+                self.topological_sort_util(i, visited, stack)
 
         stack.insert(0, v)
 
     # Topological sort function
-    def topologicalSort(self):
+    def topological_sort(self):
         visited = [False] * self.V
         stack = []
 
         for i in range(self.V):
             if (visited[i] == False):
-                self.topologicalSortUtil(i, visited, stack)
+                self.topological_sort_util(i, visited, stack)
 
         print(stack)
 
 
 g = Graph(6)
-g.addEdge(5, 2)
-g.addEdge(5, 0)
-g.addEdge(4, 0)
-g.addEdge(4, 1)
-g.addEdge(2, 3)
-g.addEdge(3, 1)
+g.add_edge(5, 2)
+g.add_edge(5, 0)
+g.add_edge(4, 0)
+g.add_edge(4, 1)
+g.add_edge(2, 3)
+g.add_edge(3, 1)
 
 print("One possible Topological Sort of the graph is:")
-g.topologicalSort()
+g.topological_sort()
