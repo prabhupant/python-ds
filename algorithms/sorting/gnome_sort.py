4f34798aa77f9691436fbeb9dbf69dc79f44d81c
Hyde46 <denis.heid@protonmail.com>
2019-10-16 21:42:43 +0200

    Add testcase for sorting

--- algorithms/sorting/gnome_sort.py
+++ algorithms/sorting/gnome_sort.py
@@ -1,7 +1,8 @@
 '''
 Gnome sort
 Similar to insertion sort, every element is set into place 
-one after another. 
+one after another. Check each element pair whether they are in place 
+or not. "Bubble" element to the left if not in place.
 
 Runtime complexity:
    Averages in O(n*n) 
@@ -24,3 +25,9 @@ def gnomeSort(array):
             pos = pos - 1
     return array
     
+
+test_array = [-19,30,11,100,-30,5,1,200,34]
+
+test_array = gnomeSort(test_array)
+
+print(test_array)
