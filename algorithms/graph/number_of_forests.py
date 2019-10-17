0401ea7edd22b67cef987ad1a68e2e590adfbd84
T SAI DEEPAK <47642563+tsaideepak7@users.noreply.github.com>
2019-10-18 00:09:06 +0530

    Update number_of_forests.py

--- algorithms/graph/number_of_forests.py
+++ algorithms/graph/number_of_forests.py
@@ -1,18 +1,4 @@
-
-
-
-#define your graph like a 2D matrix or use map function to take
-#it as input from the user
-l=[[1,0,1],
-   [1,0,1],
-   [0,1,0]]
-
-d=[[False for i in range (len(l[0]))] for i in range(len(l))]
-
-
-
 def dfs(i,j):
-    
     if(d[i][j] ==False):
         d[i][j]=True
         if(i+1<len(l) and l[i+1][j]==1):
@@ -23,12 +9,19 @@ def dfs(i,j):
             dfs(i,j+1)
         if(j-1>=0 and l[i][j-1]==1):
             dfs(i,j-1)
+            
+#define your graph like a 2D matrix or use map function to take
+#it as input from the user
+l=[[1,0,1],
+   [1,0,1],
+   [0,1,0]]
 
+d=[ [False] * len(l[0])] * len(l)
 no_forests=0
 
 for i in range(len(l)):
     for j in range(len(l[0])):
-        if d[i][j] == False and l[i][j]==1:
+        if d[i][j]==False and l[i][j]==1:
             no_forests+=1
             dfs(i,j)
 
