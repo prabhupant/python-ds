b64ef348c75659e591b28a0a7f73932ae95337f6
FARVE14 <faisalg2006@gmail.com>
2019-10-17 23:08:21 +0530

    Implementation of stack using linked list

---
+++ data_structures/stack/stack_using_linked_list.py
@@ -0,0 +1,70 @@
+""" Use LinkedList class to implement a Stack. """
+
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def insert_first(self, new_element):
+        "Insert new element as the head of the LinkedList"
+        # fetch the current head
+        current = self.head
+        new_element.next = current
+        self.head = new_element
+
+    def delete_first(self):
+        "Delete the first (head) element in the LinkedList and return it"
+        current = self.head
+        if current:
+            if current.next:
+                self.head = current.next
+            else:
+                self.head = None
+            return current
+        else:
+            return None
+
+class Stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+        "Push (add) a new element onto the top of the stack"
+        self.ll.insert_first(new_element)
+
+    def pop(self):
+        "Pop (remove) the first element off the top of the stack and return it"
+        return self.ll.delete_first()
+    
+# Test cases
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+
+# Start setting up a Stack
+stack = Stack(e1)
+
+# Test stack functionality
+stack.push(e2)
+stack.push(e3)
+print stack.pop().value
+print stack.pop().value
+print stack.pop().value
+print stack.pop()
+stack.push(e4)
+print stack.pop().value
