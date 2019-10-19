40785591bb0e65b1f77704822763310a55fd1953
deveshchatuphale7 <dcdeves3@gmail.com>
2019-10-19 17:00:36 +0530

    code as per pep8 guidlines

--- data_structures/binary_trees/binary_tree_array_implementation.py
+++ data_structures/binary_trees/binary_tree_array_implementation.py
@@ -5,41 +5,41 @@
  while right node will be stored at  (root index * 2) + 2
 '''
 
-def fillRoot(val):
-    if(treeAry[0] != None):
+def fill_root(val):
+    if(tree_ary[0] != None):
         print("Cant set root")
     else:
-        treeAry[0] = val
+        tree_ary[0] = val
     return True    
 
-def addToLeft(val,parent):
-    if(treeAry[parent] == None):
+def add_to_left(val,parent):
+    if(tree_ary[parent] == None):
         print("Cant set child")
     else:
-        treeAry[(parent * 2) + 1] = val
+        tree_ary[(parent * 2) + 1] = val
         print("Added "+ str(val))
         
 
-def addToRight(val,parent):
-    if(treeAry[parent] == None):
+def add_to_right(val,parent):
+    if(tree_ary[parent] == None):
         print("Cant set child")
     else:
-        treeAry[(parent *2 ) + 2] = val
+        tree_ary[(parent * 2) + 2] = val
         print("Added " + str(val))
    
 
-# Function to print tree in BFS manner
-def viewTree():
-    for node in treeAry:
+
+def view_tree():        # Function to print tree in BFS manner
+    for node in tree_ary:
         if(node != None):
             print(str(node))
             
 
-def findHeight():
+def find_height():
     ind = -1
     po = 0
-    for itr in range(0,len(treeAry)):
-            if(treeAry[itr] != None):
+    for itr in range(0,len(tree_ary)):
+            if(tree_ary[itr] != None):
                 ind = itr
     if(ind == -1):
         return 0
@@ -48,15 +48,15 @@ def findHeight():
            return po - 1
        po += 1
 
-if __name__=="__main__":
+if __name__ == "__main__":
     
-    treeAry = [None] * 100  
-    fillRoot(5)
-    addToLeft(2,0)
-    addToRight(3,0)
-    addToLeft(7,1)
-    # finding height of the tree
-    treeHeight = findHeight()
+    tree_ary = [None] * 100  
+    fill_root(5)
+    add_to_left(2,0)
+    add_to_right(3,0)
+    add_to_left(7,1)
+    view_tree()
+    print("Height is ",find_height())
      
 
     
