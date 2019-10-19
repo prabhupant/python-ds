4b64152bfaad9527ef023063503ba4d395d6ff80
Nick <nikosgklu@gmail.com>
2019-10-19 15:41:11 +0300

    Fixed pep8 combatability issues

--- algorithms/miscellaneous/fibonacci_recursive.py
+++ algorithms/miscellaneous/fibonacci_recursive.py
@@ -1,15 +1,17 @@
 # Basic showcase of recursion using fibonacci series
 
+
 def rec_fibo(n):
-   if n <= 1:
-       return n
-   else:
-       return(rec_fibo(n-1) + rec_fibo(n-2))
+    if n <= 1:
+        return n
+    else:
+        return(rec_fibo(n-1) + rec_fibo(n-2))
+
 
 loops = int(input("How many loops? "))
 
 if loops <= 0:
-   print("Plese enter a positive integer")
+    print("Plese enter a positive integer")
 else:
-   for i in range(loops):
-       print(rec_fibo(i))
+    for i in range(loops):
+        print(rec_fibo(i))
