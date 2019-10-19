ac609a6e6f47c5258bf7ddeb4681f200f5eac37f
Shubham Kumar Gupta <guptask009@gmail.com>
2019-10-19 12:17:06 +0530

    Added coin_change problem

---
+++ algorithms/dynamic_programming/coin_change.py
@@ -0,0 +1,34 @@
+# Python implementation of Coin Change problem
+
+
+def count_coin(S, m, n):
+    # We need n+1 rows as the table is constructed
+    # in bottom up manner using the base case 0 value
+    # case (n = 0)
+    table = [[0 for x in range(m)] for x in range(n + 1)]
+
+    # Fill the entries for 0 value case (n = 0)
+    for i in range(m):
+        table[0][i] = 1
+
+    # Fill rest of the table entries in bottom up manner
+    for i in range(1, n + 1):
+        for j in range(m):
+
+            # count_coin of solutions including S[j]
+            x = table[i - S[j]][j] if i - S[j] >= 0 else 0
+
+            # count_coin of solutions excluding S[j]
+            y = table[i][j - 1] if j >= 1 else 0
+
+            # total count_coin
+            table[i][j] = x + y
+
+    return table[n][m - 1]
+
+
+# Program to test above function
+arr = [1, 2, 3]
+m = len(arr)
+n = 4
+print(count_coin(arr, m, n))
