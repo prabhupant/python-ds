5071b68d046d3793b597cb857b1af0122a59f5df
Ed Howard <edhoward@zoho.com>
2019-10-24 14:13:53 -0400

    Use two list merge routine from merge_list_of_linked_lists.py

--- data_structures/linked_list/merge_linked_list.py
+++ data_structures/linked_list/merge_linked_list.py
@@ -6,13 +6,29 @@ class Node():
 
 
 def merge(l1, l2):
-    res = curr = Node(0)
-    while l1 and l2:
-        if l1.val < l2.val:
-            curr.next = l1
-            l1 = l1.next
-        else:
-            curr.next = l2
-            l2 = l2.next
-        curr = curr.next
-    curr.next = 
+    if not l1 and not l2:
+        return
+    elif not l2:
+        return l1
+    elif not l1:
+        return l2
+
+    if (l1.val < l2.val):
+        l1.next = merge(l1.next, l2)
+        return l1
+    l2.next = merge(l1, l2.next)
+    return l2
+
+head1 = Node(1)
+head1.next = Node(3)
+
+head2 = Node(2)
+head2.next = Node(4)
+
+head_merged = merge(head1,head2)
+
+ptr = head_merged
+while ptr:
+    print ptr.val
+    ptr = ptr.next
+
