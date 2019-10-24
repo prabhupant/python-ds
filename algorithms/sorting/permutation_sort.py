ad7930b6fd7ce1d63e64473fa027b135f6c0cb09
Pooja Gupta <gpooja@iitk.ac.in>
2019-10-24 14:08:21 +0530

    Code updated in pep8

---
+++ algorithms/sorting/permutation_sort.py
@@ -0,0 +1,36 @@
+import random
+
+# Sorts array a[0..n-1] using Bogo sort
+
+
+def bogo_sort(a):
+    n = len(a)
+    while (is_sorted(a) == False):
+        shuffle(a)
+
+# To check if array is sorted or not
+
+
+def is_sorted(a):
+    n = len(a)
+    for i in range(0, n - 1):
+        if (a[i] > a[i + 1]):
+            return False
+    return True
+
+# To generate permuatation of the array
+
+
+def shuffle(a):
+    n = len(a)
+    for i in range(0, n):
+        r = random.randint(0, n - 1)
+        a[i], a[r] = a[r], a[i]
+
+
+# Driver code to test above
+a = [3, 2, 4, 1, 0, 5]
+bogo_sort(a)
+print("Sorted array :")
+for i in range(len(a)):
+    print("%d" % a[i]),
