5187e0b1cfc9ab86df9791c08d65225f90331c2b
Nikhil Saini <43510865+nikhilnano@users.noreply.github.com>
2019-10-31 19:08:14 +0530

    Update find_missing_numbers.py

--- data_structures/array/find_missing_numbers.py
+++ data_structures/array/find_missing_numbers.py
@@ -1,3 +1,6 @@
+#The given function takes input as an array/list and find the numbers which are less than equal to the size of the
+#array and are missing in the array
+
 def find(arr):
     n = len(arr)
     index = 0
