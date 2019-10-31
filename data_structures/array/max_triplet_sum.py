50dc0856c6c963c1b7deebd92568e6ba157a7b97
Nikhil Saini <43510865+nikhilnano@users.noreply.github.com>
2019-10-31 19:21:33 +0530

    Update max_triplet_sum.py

--- data_structures/array/max_triplet_sum.py
+++ data_structures/array/max_triplet_sum.py
@@ -1,23 +1,8 @@
-import sys
-
 def max_sum(arr):
-    first = second = third = -sys.maxsize
-
-    for i in arr:
-        if i > first:
-            third = second
-            second = first 
-            first = i
-        elif i > second:
-            third = second
-            second = i
-        elif i > third:
-            third = i
-
-    return first + second + third
+    arr.sort()
+    return sum(arr[-3:-1])+arr[-1] #Sum last three elements after sorting
 
 
 arr = [1,3,6,3,6,8,3,3,7,9,4,-1,-1]
 
 print(max_sum(arr))
-
