cfe92c10d61aeb1de0fe1abf0bf27150c7ec0e72
vernalhav <vernalhav@usp.br>
2019-12-15 19:54:39 -0300

    Made LIS code simpler

--- algorithms/dynamic_programming/longest_increasing_subsequence.py
+++ algorithms/dynamic_programming/longest_increasing_subsequence.py
@@ -6,67 +6,46 @@ longest increasing subsequence
 of that array
 '''
 
-def memoize(f):
+
+def lis(s, cur_index=0, last_index=-1, cur_length=0, dp=None):
     """
-    Generic decorator for
-    Dynamic Programming
-    problems
+    Return length of the longest increasing
+    subsequence from array s.
+
+    To use, simply call lis(s). The other
+    parameters will be used in the
+    recursion
     """
-    cache = dict()
-    def memoized_f(*args):
-        if args in cache:
-            global hits
-            hits += 1
-            return cache[args]
 
-        cache[args] = f(*args)
-        return cache[args]
+    # Initialize dp dictionary to use in the recursion
+    if dp == None:
+        dp = dict()
 
-    return memoized_f
+    # Base case: we've gone through the whole array
+    if cur_index == len(s):
+        return cur_length   
 
-
-def lis(s):
-    """ Return length of LIS in array s """
+    if dp.get((last_index, cur_index)) is not None:
+        return dp.get((last_index, cur_index))  # Return stored value
 
     '''
-    NOTE: array s will be declared in the superscope
-    of lis_routine, so it doesn't need to be passed
-    as argument and subsequently not hashed by @memoize
+    Length of the LIS if we include cur_index
+    is initialized as -1 because we might not
+    be able to include it if it is less than
+    the number at last_index. If last_index is
+    -1, we can take cur_index, as it will be
+    the first integer in the possible LIS.
     '''
+    take = -1
+    if last_index == -1 or s[last_index] < s[cur_index]:
+        take = lis(s, cur_index + 1, cur_index, cur_length + 1, dp)
+    
+    # Length of the LIS if we don't include cur_index
+    not_take = lis(s, cur_index + 1, last_index, cur_length, dp)
 
-    @memoize
-    def lis_routine(cur_index=0, last_index=-1, cur_length=0):
-        """
-        Return length of the longest increasing
-        subsequence from array s.
-
-        To use, simply call lis(s). The other
-        parameters will be used in the
-        recursion
-        """
-
-        if cur_index == len(s):
-            return cur_length   
-
-        '''
-        Length of the LIS if we include cur_index
-        is initialized as -1 because we might not
-        be able to include it if it is less than
-        the number at last_index. If last_index is
-        -1, we can take cur_index, as it will be
-        the first integer in the possible LIS.
-        '''
-        take = -1
-        if last_index == -1 or s[last_index] < s[cur_index]:
-            take = lis_routine(cur_index + 1, cur_index, cur_length + 1)
-        
-        # Length of the LIS if we don't include cur_index
-        not_take = lis_routine(cur_index + 1, last_index, cur_length)
-
-        # Return the greatest LIS (include cur_index or not)
-        return max(take, not_take)
-
-    return lis_routine()
+    # Return the greatest LIS (include cur_index or not)
+    dp[(last_index, cur_index)] = max(take, not_take)   # Store result in cache
+    return dp.get((last_index, cur_index))
 
 
 def main():
