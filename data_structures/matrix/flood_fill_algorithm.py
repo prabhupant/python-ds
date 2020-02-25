db906aff3a12a9326031d81a78febdf16dbe5f43
Christian Clauss <cclauss@me.com>
2020-02-25 23:21:05 +0100

    Fix Python syntax error

--- data_structures/matrix/flood_fill_algorithm.py
+++ data_structures/matrix/flood_fill_algorithm.py
@@ -4,7 +4,7 @@
 M = 8
 N = 8
 
-def flood_fill_util(screen, x, y prev_color, new_color):
+def flood_fill_util(screen, x, y, prev_color, new_color):
     if x < 0 or x >= M or y < 0 or y >= N or screen[x][y] != prev_color or screen[x][y] == new_color:
         return
 
