9bc4cce53195f3e456f663767ee8d231ad4cad4d
lucifermorningstar <harshalparekh20696@gmail.com>
2020-05-03 04:22:08 -0500

    number of 1 in sorted array fix

--- data_structures/array/number_of_1_in_sorted_array.py
+++ data_structures/array/number_of_1_in_sorted_array.py
@@ -1,20 +1,21 @@
 # The array is sorted in decreasing order
 
-def count(arr):
+
+def count(array):
     start = 0
-    end = len(arr) - 1
+    end = len(array) - 1
 
     while start <= end:
-        mid = (start + end) // 2
-        if arr[mid] == 1 and (arr[mid + 1] == 0 or mid == high):
+        mid = start + (end - start) // 2
+        if array[mid] == 1 and (array[mid + 1] == 0 or mid == end):
             return mid + 1
-        if arr[mid] == 1:
+        if array[mid] == 1:
             start = mid + 1
         else:
             end = mid - 1
     return 0
 
 
-arr = [0,0,0,0]
+arr = [1, 1, 1, 1, 0, 0, 0, 0]
 
 print(count(arr))
