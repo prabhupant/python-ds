3376f4438f8c2085f78ada2a8aad4f883e39c807
DaPy15 <theanuempire1509@gmail.com>
2020-05-09 20:04:15 +0530

    Added euclideans method for GCD in math

--- algorithms/math/greatest_common_divisor.py
+++ algorithms/math/greatest_common_divisor.py
@@ -6,24 +6,46 @@ Time Complexity:
 O(log(n))
 """
 # Iterative Solution
-def gcd(x, y):
-	if x == 0:
-		return y
-	if y == 0:
-		return x
-	
-	while x % y != 0:
-		rem = x % y;
-		x = y
-		y = rem
 
-	return y
+
+def gcd(x, y):
+    if x == 0:
+        return y
+    if y == 0:
+        return x
+
+    while x % y != 0:
+        rem = x % y
+        x = y
+        y = rem
+
+    return y
 
 # Recursive Solution
+
+
 def gcd(x, y):
-	if x == 0:
-		return y
-	if y == 0:
-		return x
-	
-	return gcd(y, x % y)
+    if x == 0:
+        return y
+    if y == 0:
+        return x
+
+    return gcd(y, x % y)
+
+
+# Eucledean gcd algorithm
+"""
+It States that if we need to compute the gcd of x and y, We compute the equation x=qy+b in which
+q=x//y and b=x%y and we continously change the values of q and b in which the next q:=b and
+accordingly compute b. This goes on until b==0 and we take the preceding value of b.
+"""
+
+
+def gcd(K, M):
+    k = max(K, M)
+    m = min(K, M)
+    while (m != 0):
+        r = k % m
+        k = m
+        m = r
+    return k
