a9ba564b9e183e45db7c80fab12ae8fcce517b02
Prabhu Pant <prabhupant09@gmail.com>
2020-06-20 07:33:19 +0530

    Update bubble_sort.py

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -3,8 +3,20 @@ Bubble Sort worst time complexity occurs when array is reverse sorted - O(n^2)
 Best time scenario is when array is already sorted - O(n)
 """
 
-
 def bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j] 
+    return array
+
+
+def bubble_sort_optimized(array):
+    """
+    Optimizes on bubble sort by taking care of already swapped cases
+    Reference - https://github.com/prabhupant/python-ds/pull/346
+    """
     has_swapped = True
 
     num_of_iterations = 0
