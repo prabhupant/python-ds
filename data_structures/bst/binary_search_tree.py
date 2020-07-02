f134384ff46a7043c90efaa878c175a8a47c28ea
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-07-01 22:54:40 -0400

    Update binary_search_tree.py methods.

--- data_structures/bst/binary_search_tree.py
+++ data_structures/bst/binary_search_tree.py
@@ -8,31 +8,37 @@ class Node():
 
 class BST():
 
-    def __init__(self):
-        self.root = Node(None)
+    def __init__(self,root=None):
+        self.root = root
 
 
-    def insert(self, new_data):
+    def insert(self, current_node,new_data):
         if self.root is None:
             self.root = Node(new_data)
         else:
-            if self.root.val < new_data:
+            if current_node.val < new_data:
+                if current_node.right == None:
+                    current_node.right = Node(new_data)
+                else:
+                    self.insert(current_node.right, new_data)
 
-                self.insert(self.root.right, new_data)
-            else:
-                self.insert(self.root.left, new_data)
+            elif current_node.val > new_data:
+                if current_node.left == None:
+                    current_node.left = Node(new_data)
+                else:
+                    self.insert(current_node.left,new_data)
 
 
-    def inorder(self):
-        if self.root:
-            self.inorder(self.root.left)
-            print(self.root.val)
-            self.inorder(self.root.right)
+    def inorder(self,node):
+        if node is not None:
+            self.inorder(node.left)
+            print(node.val)
+            self.inorder(node.right)
 
 
 if __name__ == '__main__':
     tree = BST()
-    tree.insert(5)
-    tree.insert(4)
-    tree.insert(7)
-    tree.inorder()
+    tree.insert(tree.root,5)
+    tree.insert(tree.root,4)
+    tree.insert(tree.root,7)
+    tree.inorder(tree.root)
