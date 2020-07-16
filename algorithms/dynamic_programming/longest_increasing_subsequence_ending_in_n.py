c131d3d6984220885a294cf015b9c7c79b9a13d3
Liam Geffen <lgeffen@u.rochester.edu>
2020-07-15 22:22:11 -0400

    nit changes

--- algorithms/dynamic_programming/longest_increasing_subsequence_ending_in_n.py
+++ algorithms/dynamic_programming/longest_increasing_subsequence_ending_in_n.py
@@ -1,16 +1,16 @@
 def lis(a):
-    #n = length of array a
+    # n = length of array a. Subsequence must include a[n]
     n = len(a)
 
-    #Create table T of size n, with all entries initialized to 1
+    # Create table T of size n, with all entries initialized to 1
     T = [1]*n
 
-    #For each substring of a, find the longest increasing subsequence
-    #Set the corresponding entry of T to its value
+    # For each substring of a, find the longest increasing subsequence
+    # Set the corresponding entry of T to its value
     for i in range (1 , n):
         for j in range(0 , i):
             if T[i] <= T[j] and a[i] > a[j]:
-                T[i] = T[j]+1
+                T[i] = T[j] + 1
 
-    #Length of the longest subsequence ending with the last value in a
+    # Length of the longest subsequence ending with the last value in a
     return T[n]
