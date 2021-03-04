e5a98ba51a959892dced8a525ad9c5b800f263f8
Prabhu Pant <prabhupant09@gmail.com>
2021-03-04 09:12:16 +0530

    Add levels of nodes

---
+++ data_structures/graphs/level_of_nodes.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+class Graph:
+
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = defaultdict(list)
+
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+
+    def print_levels(self, s):
+        levels = [None] * self.vertices
+        levels[s] = 0
+        queue = []
+        queue.append(s)
+
+        while queue:
+            s = queue.pop(0)
+
+            for i in self.graph[s]:
+                if not levels[i]:
+                    levels[i] = levels[s] + 1
+                    queue.append(i)
+        
+        levels[0] = 0
+        
+        print('Node \t Level')
+        for node, level in enumerate(levels):
+            print(f'{node} \t {level}')
+
+g = Graph(8)
+g.add_edge(0, 1)  
+g.add_edge(0, 2)  
+g.add_edge(1, 3)  
+g.add_edge(1, 4)  
+g.add_edge(1, 5)  
+g.add_edge(2, 5)  
+g.add_edge(2, 6)  
+g.add_edge(6, 7) 
+g.print_levels(0)
+        
