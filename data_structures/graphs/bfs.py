0635e65a1112563552a6136e5afbd6e1840fb7e4
Prabhu Pant <prabhupant09@gmail.com>
2021-04-13 17:57:31 +0530

    Fix BFS

--- data_structures/graphs/bfs.py
+++ data_structures/graphs/bfs.py
@@ -2,8 +2,9 @@ from collections import defaultdict
 
 class Graph:
 
-    def __init__(self):
+    def __init__(self, vertices):
         self.graph = defaultdict(list)
+        self.vertices = vertices
 
 
     def add_edge(self, u, v):
@@ -11,29 +12,31 @@ class Graph:
 
 
     def bfs(self, s):
-        visited = [False] * len(self.graph)
+        visited = [False] * self.vertices
 
         queue = []
 
         queue.append(s)
         visited[s] = True
 
+        bfs = []
+
         while queue:
             s = queue.pop(0)
             print(s, end=' ')
-
             for i in self.graph[s]:
                 if visited[i] == False:
                     queue.append(i)
                     visited[i] = True
 
+g = Graph(6)
 
-g = Graph() 
-g.add_edge(0, 1) 
-g.add_edge(0, 2) 
-g.add_edge(1, 2) 
-g.add_edge(2, 0) 
-g.add_edge(2, 3) 
-g.add_edge(3, 3) 
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 3)
+g.add_edge(0, 3)
+g.add_edge(2, 4)
+g.add_edge(3, 4)
+g.add_edge(3, 5)
 
 g.bfs(0)
