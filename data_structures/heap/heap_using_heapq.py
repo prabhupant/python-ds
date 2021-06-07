1dd751bc3e99148a82c9ca034f80985e6531a0ca
Prabhu Pant <prabhupant09@gmail.com>
2021-06-07 17:47:21 +0530

    add heap using library functions

---
+++ data_structures/heap/heap_using_heapq.py
@@ -0,0 +1,23 @@
+"""
+Heap in python using heapq library function
+
+Note: by default, heapq creates a min-heap. To make it a 
+max-heap, add items after multiplying them by -1
+"""
+
+from heapq import heappop, heappush, heapify
+
+heap = []
+heapify(heap)
+
+heappush(heap, 10)
+heappush(heap, 11)
+heappush(heap, 2)
+heappush(heap, 4)
+heappush(heap, 14)
+heappush(heap, 1)
+
+print('first element - ', heap[0])
+print('popping min element - ', heappop(heap))
+print('first element - ', heap[0])
+
