2c7103f5170e7139f1ae59023d190d04ec09fa72
Prabhu Pant <prabhupant09@gmail.com>
2021-06-10 18:00:57 +0530

    Add file for hash table

---
+++ data_structures/hash/hash_table.py
@@ -0,0 +1,29 @@
+"""
+Create a hash table from scratch. Use chaining for hash collision
+"""
+
+class HashTable:
+
+
+    def __init__(self):
+        self.hash_table = 
+
+
+    def check_collision(self):
+        pass
+
+
+    def add_to_linked_list(self):
+        pass
+
+
+    def insert(self):
+        pass
+
+
+    def delete(self):
+        pass
+
+
+    def get(self):
+        pass
