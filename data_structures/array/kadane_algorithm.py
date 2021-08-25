70d2852bd4a2f752b2b9f2b7475fb8b7206450a7
Prabhu Pant <prabhupant09@gmail.com>
2021-08-25 10:10:00 +0530

    Add comments for Kadane's algorithm

--- data_structures/array/kadane_algorithm.py
+++ data_structures/array/kadane_algorithm.py
@@ -1,3 +1,12 @@
+"""
+Kadane's algorithm is used to find the maximum contiguous sum in an array.
+The logic is simple. Take the first element in the sum and then find current max num.
+Curr max = max(arr[i], curr_max + arr[i]) - we add this number if it increases the sum,
+otherwise we take the number if it is more than the sum
+
+Then keep track of max of this value
+"""
+
 def max_sum(arr):
     max_so_far = arr[0]
     curr_max = arr[0]
