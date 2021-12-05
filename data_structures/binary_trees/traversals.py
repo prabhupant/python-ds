f1f04a647be072a2e9bb91032c29f307ce6d249f
SudarshanRamanujam <sudharshanramanujam@gmail.com>
2021-12-06 01:28:35 +0530

    Added Inorder recurrsion

--- data_structures/binary_trees/traversals.py
+++ data_structures/binary_trees/traversals.py
@@ -7,9 +7,20 @@ class Node:
 
 
 class Tree:
+    """
+    Inoder Traversel(left Node,root Node,right Node)
+    """
+    def inorder(self,node):
+        if node is not None:
+           self.inorder(node.left)
+           print(node.val)
+           self.inorder(node.right)
 
-    def __init__(self, root):
-        self.root = root
 
-
-    def inorder()
+root=Node(5)
+root.left=Node(2)
+root.right=Node(7)
+root.left.left=Node(1)
+root.left.right=Node(4)
+t=Tree()
+t.inorder(root)
